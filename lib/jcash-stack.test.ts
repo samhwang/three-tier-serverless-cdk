f59c4def84dd61cdefd4d3966f8095800f6d7f33
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-28 08:50:19 +1000

    Reformat code styling

--- lib/jcash-stack.test.ts
+++ lib/jcash-stack.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from '@aws-cdk/assert';
 import cdk from '@aws-cdk/core';
 import { JCashStack } from './jcash-stack';
 
 it('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new JCashStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new JCashStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
