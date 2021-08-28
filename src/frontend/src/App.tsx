329701299e1e8a9f169c33cc59e3a4ce973928e9
Sam Huynh <samhwang2112.dev@gmail.com>
2021-08-28 17:11:12 +1000

    Use destructuring and Arrow functions in React FE

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FC } from 'react';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
