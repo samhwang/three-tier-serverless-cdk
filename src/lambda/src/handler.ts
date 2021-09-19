03c0b3c488abd389fbfcb4eec336f28ce4dbba54
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-19 10:38:59 +1000

    Add initial config for fastify and prisma

--- src/lambda/src/handler.ts
+++ src/lambda/src/handler.ts
@@ -1,7 +1,4 @@
-export const handler = async (event?: any, context?: any) => {
-  console.log({ event, context });
-  return {
-    statusCode: 200,
-    body: 'Hello World!',
-  };
-};
+import awsLambdaFastify from 'aws-lambda-fastify';
+import server from './server';
+
+export const handler = awsLambdaFastify(server);
