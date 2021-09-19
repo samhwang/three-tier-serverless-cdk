004a7aa9aef3e02209eb7286032635ce6196ebaf
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-19 11:17:47 +1000

    Add graphql routes with mercurius

--- src/lambda/src/server.ts
+++ src/lambda/src/server.ts
@@ -1,11 +1,38 @@
 import fastify from 'fastify';
+import mercurius from 'mercurius';
 
 const server = fastify();
-server.get('/', async (_, reply) =>
-  reply.send({
-    statusCode: 200,
-    body: 'Hello World!',
-  })
+
+const schema = `
+  type Query {
+    hello: HelloType
+  }
+
+  type HelloType {
+    message: String!
+    success: Boolean!
+    errors: [String]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: async () => ({
+      message: 'Hello World',
+      success: true,
+      errors: [],
+    }),
+  },
+};
+
+server.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true,
+});
+
+server.setNotFoundHandler((_, reply) =>
+  reply.code(404).send('404: Page Not Found')
 );
 
 export default server;
