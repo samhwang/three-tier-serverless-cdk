56900827d43eefad2ef0185f7c3869aea85bf2d8
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-22 23:38:05 +1000

    Install graphql packages for relay
    The relay compiler and its typescript package are installed in the main
    workspace because somehow yarn workspaces just install these 2 packages
    in 2 different places while they should be together.

---
+++ packages/frontend/relay.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  src: './src/',
+  schema: '../lambda/src/schema.graphql',
+  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
+  watchman: false,
+  language: 'typescript',
+  extensions: ['ts', 'tsx'],
+  artifactDirectory: './src/__generated__',
+};
