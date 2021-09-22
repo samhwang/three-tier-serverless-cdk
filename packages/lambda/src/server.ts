b1e09e40f6c1135e65c5b4bb22e80cb547d8c080
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-22 23:33:50 +1000

    Enable CORS on backend

--- packages/lambda/src/server.ts
+++ packages/lambda/src/server.ts
@@ -1,8 +1,14 @@
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
 import mercurius from 'mercurius';
 import schema from './graphql';
 
 const server = fastify();
+
+server.register(fastifyCors, {
+    origin: true,
+    preflightContinue: true,
+});
 
 server.register(mercurius, {
     schema,
