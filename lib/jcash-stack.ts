bb43baff19a4d0912a22e1167a24f113b994c805
Sam Huynh <samhwang2112.dev@gmail.com>
2021-09-24 23:06:52 +1000

    Move Cloudfront Declaration into the main stack

--- lib/jcash-stack.ts
+++ lib/jcash-stack.ts
@@ -1,4 +1,12 @@
 import { Stack, Construct, StackProps } from '@aws-cdk/core';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import {
+    CloudFrontWebDistribution,
+    CloudFrontAllowedMethods,
+    CloudFrontAllowedCachedMethods,
+    ViewerProtocolPolicy,
+    PriceClass,
+} from '@aws-cdk/aws-cloudfront';
 import JCashBEConstruct from './jcashBEConstruct';
 import JCashFEConstruct from './jcashFEConstruct';
 
@@ -6,6 +14,63 @@ export class JCashStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         new JCashBEConstruct(this, 'JCashBE');
-        new JCashFEConstruct(this, 'JCashFE');
+        const FEConstruct = new JCashFEConstruct(this, 'JCashFE');
+        const FEBucket = FEConstruct.bucket;
+
+        const cfDistribution = new CloudFrontWebDistribution(
+            this,
+            'JCashFEDistribution',
+            {
+                originConfigs: [
+                    {
+                        s3OriginSource: {
+                            s3BucketSource: FEBucket,
+                        },
+                        behaviors: [
+                            {
+                                isDefaultBehavior: true,
+                                compress: true,
+                                allowedMethods: CloudFrontAllowedMethods.ALL,
+                                cachedMethods:
+                                    CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
+                                forwardedValues: {
+                                    queryString: true,
+                                    cookies: {
+                                        forward: 'none',
+                                    },
+                                    headers: [
+                                        'Access-Control-Request-Headers',
+                                        'Access-Control-Request-Method',
+                                        'Origin',
+                                    ],
+                                },
+                            },
+                        ],
+                    },
+                ],
+                errorConfigurations: [
+                    {
+                        errorCode: 403,
+                        responseCode: 200,
+                        responsePagePath: '/index.html',
+                    },
+                    {
+                        errorCode: 404,
+                        responseCode: 200,
+                        responsePagePath: '/index.html',
+                    },
+                ],
+                comment: 'JCash CloudFront Distribution',
+                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                priceClass: PriceClass.PRICE_CLASS_ALL,
+            }
+        );
+
+        new BucketDeployment(this, 'DeployWithInvalidation', {
+            sources: [Source.asset('./packages/frontend/build')],
+            destinationBucket: FEBucket,
+            distribution: cfDistribution,
+            distributionPaths: ['/*'],
+        });
     }
 }
