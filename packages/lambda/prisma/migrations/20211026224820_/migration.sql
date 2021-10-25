76ccd48f83c9bb17c63a69c1cacf9e583e850a61
Sam Huynh <samhwang2112.dev@gmail.com>
2021-10-25 17:17:11 +1100

    Update sample db migration

--- packages/lambda/prisma/migrations/20211026224820_/migration.sql
+++ packages/lambda/prisma/migrations/20211026224820_/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "activated" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
