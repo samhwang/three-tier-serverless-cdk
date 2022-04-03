dba90cb48d1ef4c63abfd9d927028dc6fc2c5514
Sam Huynh <sam@hungryhungry.com>
2022-04-03 11:55:49 +1000

    Move OOP related rules into CDK eslint set

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-import baseConfig from './jest.config.base';
-
-const config: Config.InitialOptions = {
-    ...baseConfig,
-    projects: ['<rootDir>/packages/**/*'],
-};
-
-export default config;
